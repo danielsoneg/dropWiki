d32900d46c48930f84cb0770f8b3e508fd348258
danielsoneg <eric@Daedalus.local>
2011-03-12 01:11:44 -0800

    MVCish

--- dbWiki.py
+++ dbWiki.py
@@ -1,4 +1,11 @@
-import web, os, sys
+#!/usr/bin/env python
+# encoding: utf-8
+
+import web
+import os
+import sys
+import re
+import json
         
 urls = (
     '/favicon.ico','favicon',
@@ -7,55 +14,125 @@ urls = (
 app = web.application(urls, globals())
 render = web.template.render('templates')
 
-class favicon:
-    def GET(self):
-        return ''
-
 class hello:
-    def __init__(self):
-        items = os.listdir('../')
-        items = filter(lambda i: i.endswith('.txt'), items)
-        items = [i[:-4] for i in items]
-        self.textFiles = items
-    
     def POST(self, name):
         if not name:
             return "Error!"
-        else:
-            return self.writeFile(name)
+        action = web.input()['action']
+        if action == 'write': return self.write(name)
+        
+    def write(self,name):
+        content = web.input()['text']
+        f = Files.getFile(name)
+        status = f.write(content)
+        return status
     
     def GET(self, name):
         if not name:
             return self.makeIndex()
         else:
-            text = self.readFile(name)
-            if not text: text = "Not Found"
-            return text
-    
-    def writeFile(self, name):
-        try:
-            f = open("../%s.txt" % name, 'w')
-        except OSError, e:
-            return "Failed to open"
-        content = web.input()['text']
-        #return content
-        try:
-            f.write(content)
-        except:
-            return "Failed to save"
-        return "Success"
-    
-    def readFile(self, name):
-        try:
-            f = open("../%s.txt" % name)
-        except OSError, e:
-            return False
-        text = f.read()
-        f.close()
-        return render.page(name,text)
+            f = Files.getFile(name)
+            content = f.read()
+            return render.page(name,content)
     
     def makeIndex(self):
-        return render.index(self.textFiles)
+        return render.index(Files.items)
+    
+class FileModel(object):
+    def __init__(self, path='../'):
+        self.dir = path
+        self.updateItems()
+    
+    def getFile(self, name):
+        f = FileObject(name, self.dir, self.items)
+        return f
+    
+    def updateItems(self):
+        items = os.listdir(self.dir)
+        items = filter(lambda i: i.endswith('.txt'), items)
+        items = [i[:-4] for i in items]
+        self.items = items
+
+class FileObject(object):
+    def __init__(self, name, sdir,items):
+        """docstring for __init__"""
+        self.name    = name
+        self.items   = items
+        self.dir     = sdir
+        self.path    = "%s/%s.txt" % (self.dir, self.name)
+        self.content = ""
+        self.handle  = False
+    
+    def read(self):
+        if self.name in self.items:
+            self._existing()
+        else:
+            self._new()
+        return self.content
+    
+    def write(self, content):
+        if self.name not in self.items:
+            Files.updateItems()
+            self.items = Files.items
+        if content == self.content:
+            return self._success(self.content)
+        else:
+            self.content = content
+            self._preSave()
+            self.content = reUnlink.sub("`\\1`", self.content)
+            self.handle = open(self.path,'w')
+            try:
+                self.handle.write(self.content)
+            except OSError, e:
+                return self._error("Could not write file")
+            self.handle.close()
+            self._addLinks()
+            return self._success(self.content)
+            
+    def _existing(self):
+        if os.path.isfile(self.path):
+            self.handle = open(self.path)
+            self.content = self.handle.read()
+            self.handle.close()
+            self._addLinks()
+            self.handle.close()
+        """docstring for existing"""
+
+    def _new(self):
+        self.content=""
+
+    def _preSave(self):
+        """docstring for _preSave"""
+        content = self.content
+        content = content.replace('<br/>', '\n')
+        content = content.replace('<div><br>', '\n')
+        content = content.replace('<br>', '\n')
+        content = content.replace('</div><div>','\n')
+        content = content.replace('<div>','\n')
+        content = content.replace('</div>','')
+        content = reUnspan.sub("\\1", content)
+        self.content = content
+
+    def _addLinks(self):
+        self.content = reLink.sub("<a href='\\1'>\\1</a>", self.content,0)
+    
+    def _error(self,message):
+        code = {}
+        code['Code'] = 0
+        code['Message'] = message
+        return code
+    
+    def _success(self,message):
+        code = {}
+        code['Code'] = 1
+        code['Message'] = message
+        return json.dumps(code)
+    
+
+reLink   = re.compile(r'`(.*?)`', re.U)
+reUnlink = re.compile(r'<a href=\".*?">(.*?)</a>', re.U)
+reUnspan = re.compile(r'<span .*?>(.*?)</span>', re.MULTILINE)
+Files = FileModel()
 
 if __name__ == "__main__":
     app.run()
--- static/js/jquery.edit.js
+++ static/js/jquery.edit.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
+    $('a').click(function(){ window.location=$(this).attr('href'); });
     $('#editable').blur(function() {
-        var newValue = $(this).text();
+        var newValue = $(this).html();
+        var that = this;
         // do something         
         // For example, you could place an AJAX call here:  
+        alert("Sending: " +newValue);
         $.ajax({
            type: "POST",
            url: window.location.pathname,
-           data: "text=" + newValue,
-           success: function(msg){
-             alert( "Data Saved: " + msg );
+           data: "text=" + newValue + "&action=write",
+           success: function(data) {
+               data = jQuery.parseJSON(data);
+               alert(data.Message);
+               $(that).html(data.Message);
+               $('a').click(function(){ window.location=$(this).attr('href'); });
            }
         });
     });
