27b2fadb139b6cf0d151e47612b0c7a2eed896d5
danielsoneg <eric@Daedalus.local>
2011-03-15 13:17:42 -0700

    Add handling for external links

--- files.py
+++ files.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 import os
 import re
 try: import simplejson as json
@@ -28,6 +31,7 @@ class FileObject(object):
         self.content = ""
         self.handle  = False
     
+    # Main Commands
     def read(self):
         if self.name in self.items:
             self.__existing()
@@ -42,7 +46,7 @@ class FileObject(object):
         else:
             self.content = content
             self.__preSave()
-            self.content = reUnlink.sub("`\\1`", self.content)
+            self.__stripLinks()
             self.handle = open(self.path,'w')
             try:
                 self.handle.write(self.content)
@@ -64,6 +68,7 @@ class FileObject(object):
         self.path = newPath
         return self.__success("Renamed",{'oldURL':oldName,'newURL':newName});
     
+    # Helpers
     def __existing(self):
         if os.path.isfile(self.path):
             self.handle = open(self.path)
@@ -87,9 +92,36 @@ class FileObject(object):
         content = reUnspan.sub("\\1", content)
         self.content = content
     
+
+    # Link Handling
     def __addLinks(self):
-        self.content = reLink.sub("<a href='\\1'>\\1</a>", self.content,0)
+        self.__stripLinks()
+        self.__webLinks()
+        self.__pageLinks()
+
+    def __stripLinks(self):
+        self.content = reUnlink.sub(self.__stripLinks_linkType, self.content)
+
+    def __stripLinks_linkType(self, link):
+        if link.group(1).startswith('http'):
+            return link.group(2)
+        else:
+            return "`%s`" % link.group(2)
+
+    def __pageLinks(self):
+        self.content = reLink.sub('<a href="\\1">\\1</a>', self.content,0)
     
+    def __webLinks(self):
+        self.content = reHref.sub(self.__webLinks_fixHref, self.content, 0)
+    
+    def __webLinks_fixHref(self, link):
+        url = link.group(0)
+        href = url
+        if not url.startswith('http'):
+            href = "http://%s" % url
+        return '<a href="%s">%s</a>' % (href, url)
+    
+    #Returns
     def __error(self,message,code={}):
         code['Code'] = 0
         code['Message'] = message
@@ -103,5 +135,6 @@ class FileObject(object):
 
 
 reLink   = re.compile(r'`(.*?)`', re.U)
-reUnlink = re.compile(r'<a href=\".*?">(.*?)</a>', re.U)
+reUnlink = re.compile(r'<a href="(.*?)">(.*?)</a>', re.U)
 reUnspan = re.compile(r'<span .*?>(.*?)</span>', re.MULTILINE)
+reHref   = re.compile(r'''(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))''', re.U)
